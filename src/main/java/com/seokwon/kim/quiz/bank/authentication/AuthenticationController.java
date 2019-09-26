3a42119b0e46296e9fe164a305ad6ef9c16bcfdf
KimSeokWon <seokwon.kim@sicc.co.kr>
2019-09-27 01:33:52 +0900

    인증을 위한 단위테스트 추가

---
+++ src/main/java/com/seokwon/kim/quiz/bank/authentication/AuthenticationController.java
@@ -0,0 +1,4 @@
+package com.seokwon.kim.quiz.bank.authentication;
+
+public class AuthenticationController {
+}
---
+++ src/main/java/com/seokwon/kim/quiz/bank/authentication/service/AuthenticationService.java
@@ -0,0 +1,4 @@
+package com.seokwon.kim.quiz.bank.authentication.service;
+
+public class AuthenticationService {
+}
